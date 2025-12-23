c10bf80615843c8ea93ad4ffce2245bb3183a1a4
mrafiansyahm <mrafiansyahmahardika@gmail.com>
2025-12-23 15:37:55 +0700

    Update portofolio.py

--- portofolio.py
+++ portofolio.py
@@ -13,3 +13,19 @@ tab1, tab2, tab3, tab4, tab5 = st.tabs(["About Dataset",
                                        "Contact Me"])
 
 df = pd.read_csv("healthcare-dataset-stroke-data.csv")
+
+with tab1:
+    import about
+    about.about_dataset()
+with tab2:
+    import visualisasi
+    visualisasi.chart()
+with tab3:
+    import machine_learning
+    machine_learning.ml_model()
+with tab4:
+    import prediction
+    prediciton.prediction_app()
+
+def about_dataset():
+    st.write("**Tentang Dataset**")
